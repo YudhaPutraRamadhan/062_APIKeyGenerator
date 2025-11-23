90d6c2af79f45970f4ad0f22f8be481a2be395ea
MzSuwawa <yudharamadhan838@gmail.com>
2025-11-24 01:10:53 +0900

    Menambahkan middleware autentikasi JWT

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: 'Tidak ada token, otorisasi ditolak' });
+  }
+
+  try {
+    const token = authHeader.split(' ')[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.admin = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Token tidak valid' });
+  }
+};
